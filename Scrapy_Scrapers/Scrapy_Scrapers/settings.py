eabce604e6c024938c0a20edda966f022a728b41
ColinKlich <115095065+ColinKlich@users.noreply.github.com>
2023-08-02 14:50:01 -0500

    user-agent update

--- Scrapy_Scrapers/Scrapy_Scrapers/settings.py
+++ Scrapy_Scrapers/Scrapy_Scrapers/settings.py
@@ -14,10 +14,10 @@ NEWSPIDER_MODULE = "Scrapy_Scrapers.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "Scrapy_Scrapers (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
